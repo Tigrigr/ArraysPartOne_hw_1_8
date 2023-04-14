c58840537bb408b3f3c050b91ed9b2f140f5b2b4
mikha <Mikhailov.dzenphone@gmail.com>
2023-04-14 13:36:03 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] arraysInt = {1, 2, 3};
+        double[] arraysDouble = {1.57, 7.654, 9.986};
+        int[] arraysRandom = {4, 5, 6};
     }
 }
